464bd5c4d0e6061875e8563f667fb3bc6c0a7b9c
John Heitmann <jheitmann@gmail.com>
2020-06-23 19:02:20 -0700

    About to convert to XCode 12

--- SwiftUI Rich Text/AppDelegate.swift
+++ SwiftUI Rich Text/AppDelegate.swift
@@ -11,7 +11,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
+        let contentView = BasicRichTextView()
 
         // Create the window and set the content view. 
         window = NSWindow(
--- SwiftUI Rich Text/ContentView.swift
+++ SwiftUI Rich Text/ContentView.swift
@@ -1,17 +1,85 @@
-//
-
 import SwiftUI
 
-struct ContentView: View {
+// MARK: - A basic hardcoded rich text example
+
+struct BasicRichTextView: View {
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        (
+            Text("Bold").bold() +
+            Text("ly going").italic() +
+            Text(" where new lines have gone").foregroundColor(.red) +
+            Text(" before.").font(.headline)
+        )
+        .padding()
+        .frame(maxWidth: 200, maxHeight: 100)
     }
 }
 
+// MARK: - Demo of programmatic rich text
 
-struct ContentView_Previews: PreviewProvider {
+enum RichTextBlock: Hashable {
+    case plainTextBlock([InlineText])
+    case quote([RichTextBlock])
+}
+
+struct InlineText: Hashable {
+    let text: String
+    let attributes: TextAttributes
+}
+
+struct TextAttributes: OptionSet, Hashable {
+    let rawValue: Int
+
+    static let bold    = TextAttributes(rawValue: 1 << 0)
+    static let italic  = TextAttributes(rawValue: 1 << 1)
+    static let heading   = TextAttributes(rawValue: 1 << 2)
+}
+
+struct ProgrammaticRichTextView: View {
+    let topBlocks: [RichTextBlock]
+    
+    var body: some View {
+        VStack {
+            ForEach(topBlocks, id: \.self) { block in
+                TextBlockView(block: block)
+            }
+        }
+    }
+}
+
+struct TextBlockView: View {
+    let block: RichTextBlock
+    
+    var body: some View {
+       /* VStack {
+            switch block {
+            case .plainTextBlock(let text):
+                renderInlineText(text)
+            case .quote(let quote):
+                renderQuote(quote)
+            }
+        }*/
+        Text("Temp")
+    }
+    
+    func renderInlineText(_ text: [InlineText]) -> some View {
+        return Text("Inline text")
+    }
+
+    func renderQuote(_ quote: [RichTextBlock]) -> some View {
+        return Text("Quote text")
+    }
+}
+
+let demoBlocks: [RichTextBlock] = [
+    .plainTextBlock([InlineText(text: "foo", attributes: [.bold])])
+]
+
+struct RichTextView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            BasicRichTextView()
+            ProgrammaticRichTextView(topBlocks: demoBlocks)
+        }
     }
 }
