4d071915e0d7ff2835b5d4ea431b8540add9c606
John Heitmann <jheitmann@gmail.com>
2020-06-23 18:21:43 -0700

    Initial Commit

---
+++ SwiftUI Rich Text/ContentView.swift
@@ -0,0 +1,17 @@
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
